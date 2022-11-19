f104caaa2d3fcf2ec401f6c04656a8dbc360bf1c
Peter Varga <pkvarg@yahoo.se>
2022-11-19 13:24:06 +0100

    GoogleIdentity Front

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -6,9 +6,7 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import FormContainer from '../components/FC'
 import { login, getGoogleUserInfo } from '../actions/userActions'
-// import GoogleLogin from 'react-google-login'
-// import { gapi } from 'gapi-script'
-
+import jwt_decode from 'jwt-decode'
 const LoginScreen = () => {
   const navigate = useNavigate()
 
@@ -34,62 +32,76 @@ const LoginScreen = () => {
     dispatch(login(email, password))
   }
 
-  const [loginData, setLoginData] = useState(
-    localStorage.getItem('loginData')
-      ? JSON.parse(localStorage.getItem('loginData'))
-      : null
-  )
-  // useEffect(() => {
-  //   function start() {
-  //     gapi.client.init({
-  //       clientId:
-  //         '336257746111-nc1cme4gda545cohoqgbmsbq3hsr048k.apps.googleusercontent.com',
-  //       scope: 'email',
-  //     })
-  //   }
+  const [user, setUser] = useState({})
 
-  //   gapi.load('client:auth2', start)
-  // }, [])
+  const handleCallbackResponse = (response) => {
+    console.log('Encoded JWT ID token: ' + response.credential)
+    let userObject = jwt_decode(response.credential)
+    console.log(userObject)
+    setUser(userObject)
+    document.getElementById('signInDiv').hidden = true
+  }
 
+  const handleSignOut = (event) => {
+    setUser({})
+    document.getElementById('signInDiv').hidden = false
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: process.env.REACT_APP_GOOGLE_ID,
+      callback: handleCallbackResponse,
+    })
+
+    google.accounts.id.renderButton(document.getElementById('signInDiv'), {
+      theme: 'outline',
+      size: 'large',
+    })
+
+    google.accounts.id.prompt()
+  }, [])
+  // If no user: sign in btn
+  // If user: log out btn
   const handleFailure = (result) => {
     console.log(result)
     alert(result)
   }
 
-  const handleLogin = async (googleData) => {
-    console.log(googleData)
+  // const handleLogin = async (googleData) => {
+  //   console.log(googleData)
 
-    const res = await fetch('/api/google-login', {
-      method: 'POST',
-      body: JSON.stringify({
-        token: googleData.tokenId,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
+  //   const res = await fetch('/api/google-login', {
+  //     method: 'POST',
+  //     body: JSON.stringify({
+  //       token: googleData.tokenId,
+  //     }),
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //   })
 
-    const data = await res.json()
-    setLoginData(data)
-    localStorage.setItem('loginData', JSON.stringify(data))
-    // G
-    console.log(googleData.profileObj.email, googleData.googleId)
-    data.googleId = googleData.googleId
-    //data._id = googleData.googleId
-    data.token = googleData.tokenId
-    data.isAdmin = false
-    // localStorage.setItem('userInfo', JSON.stringify(data))
-    dispatch(getGoogleUserInfo(data))
-    console.log(userInfo)
-  }
-  const handleLogout = () => {
-    localStorage.removeItem('loginData')
-    setLoginData(null)
-    // G
-    // const x = localStorage.getItem('userInfo')
-    // console.log(x)
-    //localStorage.removeItem('userInfo')
-  }
+  //   const data = await res.json()
+  //   setLoginData(data)
+  //   localStorage.setItem('loginData', JSON.stringify(data))
+  //   // G
+  //   console.log(googleData.profileObj.email, googleData.googleId)
+  //   data.googleId = googleData.googleId
+  //   //data._id = googleData.googleId
+  //   data.token = googleData.tokenId
+  //   data.isAdmin = false
+  //   // localStorage.setItem('userInfo', JSON.stringify(data))
+  //   dispatch(getGoogleUserInfo(data))
+  //   console.log(userInfo)
+  // }
+  // const handleLogout = () => {
+  //   localStorage.removeItem('loginData')
+  //   setLoginData(null)
+  //   // G
+  //   // const x = localStorage.getItem('userInfo')
+  //   // console.log(x)
+  //   //localStorage.removeItem('userInfo')
+  // }
 
   return (
     <FormContainer>
@@ -120,24 +132,24 @@ const LoginScreen = () => {
           Sign In
         </Button>
 
-        {/* <h1>Google Log in</h1>
-        <div>
-          {loginData ? (
-            <div>
-              <h3>You logged in as {loginData.email}</h3>
-              <button onClick={handleLogout}>Logout</button>
-            </div>
-          ) : (
-            <GoogleLogin
-              clientId='336257746111-nc1cme4gda545cohoqgbmsbq3hsr048k.apps.googleusercontent.com'
-              buttonText={'Log in with Google'}
-              onSuccess={handleLogin}
-              onFailure={handleFailure}
-              cookiePolicy={'single_host_origin'}
-              plugin_name='WebAppProShop'
-            ></GoogleLogin>
-          )}
-        </div> */}
+        <div id='signInDiv'></div>
+
+        {user && (
+          <div className='my-3'>
+            <img src={user.picture}></img>
+            <h3>{user.name}</h3>
+          </div>
+        )}
+
+        {Object.keys(user).length != 0 && (
+          <Button
+            className='my-1'
+            variant='primary'
+            onClick={(e) => handleSignOut(e)}
+          >
+            Google Sign Out
+          </Button>
+        )}
       </Form>
 
       <Row className='py-3'>
